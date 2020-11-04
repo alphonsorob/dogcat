f357824deb9f06a09608721c2d95897466ac7834
Robin Trakroo <robin.trakroo@gmail.com>
2020-11-04 19:53:07 +0530

    4nov

--- cnn.py
+++ cnn.py
@@ -1,3 +1,4 @@
+'''
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -24,10 +25,10 @@ classifier.add(Flatten())
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
 
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 4, activation = 'softmax'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
@@ -39,15 +40,15 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('/Users/sudhanshukumar/Downloads/PetImages-20190330T113641Z-001/PetImages',
+training_set = train_datagen.flow_from_directory('/home/robin/ai/ineuron/main/3_computer_vision/dogcat/imgclf/images',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 class_mode = 'sparse')
 
-test_set = test_datagen.flow_from_directory('/Users/sudhanshukumar/Downloads/PetImages-20190330T113641Z-001/PetImages',
+test_set = test_datagen.flow_from_directory('/home/robin/ai/ineuron/main/3_computer_vision/dogcat/imgclf/images',
                                             target_size = (64, 64),
                                             batch_size = 32,
-                                            class_mode = 'binary')
+                                            class_mode = 'sparse')
 
 model = classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
@@ -55,24 +56,32 @@ model = classifier.fit_generator(training_set,
                          validation_data = test_set,    
                          validation_steps = 2000)
 
-classifier.save("model.h5")
+classifier.save("model1.h5")
 print("Saved model to disk")
-
+print(training_set.class_indices)
 # Part 3 - Making new predictions
 
-
+'''
 
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('/Users/sudhanshukumar/Downloads/cat.11.jpg', target_size = (64, 64))
+import keras
+test_image = image.load_img('/home/robin/ai/apps/solarsys/test_images/saturn01.jpeg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-training_set.class_indices
+new_model = keras.models.load_model('model1.h5')
+result = new_model.predict(test_image)
+#training_set.class_indices
 if result[0][0] == 1:
-    prediction = 'dog'
+    prediction = 'earth'
     print(prediction)
-else:
-    prediction = 'cat'
+elif result[0][1] == 1:
+    prediction = 'jupiter'
+    print(prediction)
+elif result[0][2] == 1:
+    prediction = 'mars'
+    print(prediction)
+elif result[0][3] == 1:
+    prediction = 'saturn'
     print(prediction)
--- predict.py
+++ predict.py
@@ -17,7 +17,7 @@ class dogcat:
 
     def predictiondogcat(self):
         # load model
-        model = load_model('model.h5')
+        model = load_model('model1.h5')
 
         # summarize model
         #model.summary()
@@ -27,11 +27,16 @@ class dogcat:
         test_image = np.expand_dims(test_image, axis = 0)
         result = model.predict(test_image)
 
+
         if result[0][0] == 1:
-            prediction = 'dog'
+            prediction = 'earth'
             return [{ "image" : prediction}]
-        else:
-            prediction = 'cat'
+        elif result[0][1] == 1:
+            prediction = 'jupiter'
             return [{ "image" : prediction}]
-
-
+        elif result[0][2] == 1:
+            prediction = 'mars'
+            return [{ "image" : prediction}]
+        elif result[0][3] == 1:
+            prediction = 'saturn'
+            return [{ "image" : prediction}]
